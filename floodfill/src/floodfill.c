b8bb48a45348906ea39f12e8f6f809ea35620b0c
Lucas <lucasrsilva.dev@gmail.com>
2022-02-13 23:54:57 -0300

    Update floodfill.c

--- floodfill/src/floodfill.c
+++ floodfill/src/floodfill.c
@@ -3,13 +3,12 @@
 // ----------------------------- FLOODFILL BFS
 
  
- // check if it is possible to go to pixel (x, y) from the
+// check if it is possible to go to pixel (x, y) from the
 // current pixel. The function returns false if the pixel
 // has a different color, or it's not a valid pixel
 bool isSafe(Graph G, int x, int y, char target, char replacement) {
     Vertex v = G->adjMatrix[x][y];
     if((x >= 0 && x < 10) && (y >= 0 && y < 10) && (v->value == target)) {
-/*      printf("\nx = %d , y = %d , v->value = %c", x, y, v->value);stop();*/
       v->value = replacement;
       return true;
     }
@@ -40,10 +39,6 @@ void floodfill(Graph G, int x, int y, char replacement) {
         Item *u = Dequeue(f);
         
         // (x, y) represents the current pixel 
-/*        Vertex v = G->adjMatrix[u->x][u->y];*/
-/*        printf("Dequeue = %d %d", u->x, u->y);*/
-        // replace the current pixel color with that of replacement
-/*        v->value = replacement;*/
         printf("\n\n");
         ImprimeGraph(G);
         stop();
@@ -53,7 +48,6 @@ void floodfill(Graph G, int x, int y, char replacement) {
             if (isSafe(G, u->x + row[k], u->y + col[k], target, replacement)) {
                 // enqueue adjacent pixel
                 Queue(f, u->x + row[k], u->y + col[k]);
-/*                printf("\nf->size = %d row %d col %d", f->size, row[k], col[k]);stop();*/
             }
         }
     }
@@ -165,86 +159,4 @@ Item* Dequeue(Fila *f){
   f->size --;
 
   return aux;
-}
-
-// ----------------- BFS
-
-/*void BFS(Graph G, int s){ //u vértice do vetor; s vertice que se deseja iniciar a pesquisa*/
-/*  int cor[G->V];*/
-/*  int d[G->V];*/
-/*  int *pi = (int*) malloc(G->V * sizeof(int));*/
-/*  Fila *f = FFVazia();*/
-/*  */
-/*  for(int i=0; i<G->V; i++) {*/
-/*    if(i != s){*/
-/*      cor[i] = 0;*/
-/*      d[i]   = -1;*/
-/*      pi[i]  = -1;*/
-/*    }*/
-/*  }*/
-
-/*  cor[s] = 1;*/
-/*  d[s]   = 0;*/
-/*  pi[s]  = -1;*/
-
-/*  Queue(f, s);*/
-
-/*  while(f->size > 0) {*/
-/*    Item *u = Dequeue(f);*/
-/*    for(int j=0; j<G->V; j++) {*/
-/*      Vertex v = G->adjMatrix[u->data][j];*/
-/*      if(v->value == 1 && cor[j] == 0) {*/
-/*        cor[j] = 1;*/
-/*        d[j]   = d[u->data] + 1;*/
-/*        pi[j]  = u->data;*/
-/*        Queue(f, j);*/
-/*      }*/
-/*    }*/
-/*    cor[u->data] = 2;*/
-/*    printf("Vertex:%d\n", u->data);*/
-/*    printf("cor:%d\n", cor[u->data]);*/
-/*    printf("pai:%d\n", pi[u->data]);*/
-/*    printf("distância:%d\n", d[u->data]);*/
-/*    printf("\n");*/
-/*  }*/
-/*  free(pi);*/
-/*}*/
-
-// ---------------------- DFS
-
-// branco: v não visitado/conhecido 
-// cinza: v conhecido mas não visitado
-// preto: v conhecido e visitado
-
-// void DFS(Graph G, int s){ 
-//   int cor[G->V]; // Branco 0, Cinza 1, Preto 2
-//   int d[G->V];   // Tempo de descoberta
-//   int f[G->V];   // Tempo de finalização
-//   int tempo = 0;
-
-//   for(int v=0; v<G->V; v++)
-//     cor[v] = 0;
-  
-//   DFS_VISIT(G, s, cor, d, f, &tempo);
-// }
-
-// void DFS_VISIT(Graph G, int j, int *cor, int *d, int *f, int *tempo){
-//   cor[j]  = 1;
-//   *tempo  += 1;
-//   d[j]    = *tempo;
-
-//   for(int k=0; k<G->V; k++) {
-//     Vertex u = G->adjMatrix[j][k];
-//     if(u->value == 1 && cor[k] == 0)
-//       DFS_VISIT(G, k, cor, d, f, tempo);
-//   }
-
-//   cor[j] = 2;
-//   *tempo += 1;
-//   f[j] = *tempo;
-//   printf("Vertex:%d D:%d, F:%d \n", j, d[j], f[j]);
-// }
-
-void stop() {
-  printf("\nSTOP...");getchar();while(getchar() != '\n');
 }
